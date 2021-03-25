e70af670968355218d7982c12821f06ad727c194
Alejandro R. Mosteo <amosteo@unizar.es>
2021-03-25 23:16:39 +0100

    Take ANSI codes into account for Text_IO.Format_Paragraph

---
+++ src/aaa-ansi.ads
@@ -0,0 +1,16 @@
+with Ada.Strings.UTF_Encoding;
+
+package AAA.ANSI with Preelaborate is
+
+   package UTF renames Ada.Strings.UTF_Encoding;
+
+   function Count_Extra (Text : Wide_Wide_String) return Natural;
+   --  Compute how many characters in Text are actually ANSI control sequences
+
+   function Length (Text : Wide_Wide_String) return Natural;
+   --  Compute the real length of Text, without embedded ANSI control sequences
+
+   function Length (Text : UTF.UTF_String) return Natural;
+   --  Same, but expects latin-1 or UTF-8 encoding
+
+end AAA.ANSI;
