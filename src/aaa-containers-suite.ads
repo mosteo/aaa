a3bdabd5a9f59015f4042c8b794269f0d47a76d8
Alejandro R. Mosteo <amosteo@unizar.es>
2025-01-21 16:19:15 +0100

    Containers suite

---
+++ src/aaa-containers-suite.ads
@@ -0,0 +1,22 @@
+with Ada.Containers.Indefinite_Ordered_Maps;
+with Ada.Containers.Indefinite_Ordered_Sets;
+with Ada.Containers.Indefinite_Vectors;
+
+generic
+   type Keys (<>) is private;
+   type Stored (<>) is private;
+   with function "<" (L, R : Keys) return Boolean is <>;
+   with function "<" (L, R : Stored) return Boolean is <>;
+package AAA.Containers.Suite with Preelaborate is
+
+   --  A collection of ready-to-use containers with conversions among them
+
+   package Maps is new Ada.Containers.Indefinite_Ordered_Maps (Keys, Stored);
+   package Sets is new Ada.Containers.Indefinite_Ordered_Sets (Stored);
+   package Vecs is new Ada.Containers.Indefinite_Vectors (Positive, Stored);
+
+   -------------------
+   --  Conversions  --
+   -------------------
+
+end AAA.Containers.Suite;
